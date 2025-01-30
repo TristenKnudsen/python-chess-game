ff65f37b9800c2273c9ee5fb2e12c98ee8a8663e
Tristen Knudsen <tristen.knudsen@gmail.com>
2025-01-30 18:58:42 -0500

    added a check to validate pawn moves

--- src/board.py
+++ src/board.py
@@ -1,10 +1,11 @@
+from src.pieces import Pawn
 class Board:
     def __init__(self):
        self.board = [[None for x in range(8)] for y in range(8)]
        self.setupPieces()
     def setupPieces(self):
         self.board[0] = ["R","N","B","Q","K","B","N","R"]
-        self.board[1] = ["P"]*8
+        for col in range(8): self.board[1][col] = Pawn("white")
         self.board[6] = ["p"]*8
         self.board[7] = ["r","n","b","q","k","b","n","r"]
     def printBoard(self):
@@ -19,10 +20,17 @@ class Board:
         self.board[endx][endy] = self.board[startx][starty]
         self.board[startx][starty] = None
         board.printBoard()
+        
+    def movePiece1(self, sx,sy, ex,ey):
+        print(self.board[sx][sy].canMove(self.board, sx,sy, ex, ey))
+        
+    def addPiece(self,locX,locY):
+        self.board[locX][locY] = "0"
     
     
         
 board = Board()
-board.movePiece(1,0,2,0)
-print("\n")
-board.movePiece(0,1,2,2)
+board.movePiece1(1,0,3,0)
+#print("\n")
+#board.addPiece(3,2)
+#board.movePiece(0,1,2,2)
--- src/pieces.py
+++ src/pieces.py
@@ -1,3 +1,20 @@
-class Pieces:
-    def __init__(self):
+class Piece:
+    def __init__(self, colour: str):
+        self.colour = colour
+        self.symbol = ""
+        self.hasMoved = False
+
+class Pawn(Piece):
+    def __init__(self,colour):
+        super().__init__(colour)
+        self.symbol = "P"
+    def __repr__(self):
+        return self.symbol.upper() if self.colour == "white" else self.symbol.lower()
         
+    def canMove(self,board,sx,sy, ex,ey):
+        if (sy == ey) and ey == (sy +1) or ey == (sy+2):
+            return True
+        else:
+            return False
+            
+    
