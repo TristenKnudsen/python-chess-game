20c25ffa90371135ca7f40ff4e47cc014939cde4
Tristen Knudsen <tristen.knudsen@gmail.com>
2025-01-30 18:07:22 -0500

    Added function in board to move pieces. Need to create piece object

--- src/board.py
+++ src/board.py
@@ -1,16 +1,28 @@
 class Board:
     def __init__(self):
-       self.board = [[0 for x in range(8)] for y in range(8)]
-       self.setup_pieces()
-    def setup_pieces(self):
+       self.board = [[None for x in range(8)] for y in range(8)]
+       self.setupPieces()
+    def setupPieces(self):
         self.board[0] = ["R","N","B","Q","K","B","N","R"]
         self.board[1] = ["P"]*8
         self.board[6] = ["p"]*8
         self.board[7] = ["r","n","b","q","k","b","n","r"]
-    def get_piece(self, row:int, col:int) -> str |None:
+    def printBoard(self):
+        for c in self.board:
+            formattedRow = [str(piece) if piece else "." for piece in c]
+            print(" ".join(formattedRow))
+            
+    def getPiece(self, row:int, col:int) -> str |None:
         return self.board[row][col]
         
-board1 = Board()
-
-print(board1.get_piece(0,0))
-
+    def movePiece(self, startx:int,starty:int, endx:int, endy:int):
+        self.board[endx][endy] = self.board[startx][starty]
+        self.board[startx][starty] = None
+        board.printBoard()
+    
+    
+        
+board = Board()
+board.movePiece(1,0,2,0)
+print("\n")
+board.movePiece(0,1,2,2)
