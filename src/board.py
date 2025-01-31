1766499a91bf6d818c1c3834e098861dadf22753
Tristen Knudsen <tristen.knudsen@gmail.com>
2025-01-31 01:14:49 -0500

    find all pawn moves determined

--- src/board.py
+++ src/board.py
@@ -1,13 +1,14 @@
-from src.pieces import Pawn
+from pieces import Pawn
+
 class Board:
     def __init__(self):
        self.board = [[None for x in range(8)] for y in range(8)]
-       self.setupPieces()
-    def setupPieces(self):
-        self.board[0] = ["R","N","B","Q","K","B","N","R"]
-        for col in range(8): self.board[1][col] = Pawn("white")
-        self.board[6] = ["p"]*8
-        self.board[7] = ["r","n","b","q","k","b","n","r"]
+       #self.setupPieces()
+    #def setupPieces(self):
+        #self.board[0] = ["R","N","B","Q","K","B","N","R"]
+        #for col in range(8): self.board[1][col] = Pawn("white",1,col)
+        #self.board[6] = ["p"]*8
+        #self.board[7] = ["r","n","b","q","k","b","n","r"]
     def printBoard(self):
         for c in self.board:
             formattedRow = [str(piece) if piece else "." for piece in c]
@@ -15,22 +16,44 @@ class Board:
             
     def getPiece(self, row:int, col:int) -> str |None:
         return self.board[row][col]
+    
+    def inBounds(row, col):
+        return 0 <= row <= 7 and 0 <= col <= 7
         
     def movePiece(self, startx:int,starty:int, endx:int, endy:int):
         self.board[endx][endy] = self.board[startx][starty]
         self.board[startx][starty] = None
         board.printBoard()
-        
-    def movePiece1(self, sx,sy, ex,ey):
-        print(self.board[sx][sy].canMove(self.board, sx,sy, ex, ey))
-        
-    def addPiece(self,locX,locY):
-        self.board[locX][locY] = "0"
-    
     
         
+    #def movePiece1(self, sx,sy, ex,ey):
+    #    print(self.board[sx][sy].canMove(self.board, sx,sy, ex, ey))
+        
+    def addPiece(self,piece):
+        self.board[piece.row][piece.col] = piece
+    
+    
+#create list to hold white pieces and black pieces
+
 board = Board()
-board.movePiece1(1,0,3,0)
+board.addPiece(Pawn(-1,5,5))
+board.addPiece(Pawn(-1,5,4))
+board.addPiece(Pawn(1,5,6))
+board.getPiece(5,4).potentialenPassant = True
+
+
+#board.addPiece(Pawn(-1,1,2))
+#board.addPiece(Pawn(-1,1,0))
+#board.getPiece(1,2).enPassant = True;
+#board.getPiece(1,0).enPassant = False;
+
+#board.addPiece(Pawn(-1,2,1))
+#board.addPiece(Pawn(-1,2,2))
+#board.addPiece(Pawn(1,3,1))
+#board.addPiece(Pawn(-1,1,0))
+board.printBoard();
+print(board.getPiece(5,5).moves(board.board))
+#board.movePiece1(1,0,3,0)
 #print("\n")
 #board.addPiece(3,2)
 #board.movePiece(0,1,2,2)
--- src/pieces.py
+++ src/pieces.py
@@ -1,20 +1,95 @@
 class Piece:
-    def __init__(self, colour: str):
-        self.colour = colour
+    def __init__(self, colour: int, row :int, col:int):
+        self.colour = colour # 1 for white, -1 for black
         self.symbol = ""
         self.hasMoved = False
+        self.row = row
+        self.col = col
+    
+    
 
 class Pawn(Piece):
-    def __init__(self,colour):
-        super().__init__(colour)
+    def __init__(self, colour: str, row: int ,col: int):
+        super().__init__(colour,row,col)
         self.symbol = "P"
-    def __repr__(self):
-        return self.symbol.upper() if self.colour == "white" else self.symbol.lower()
+        self.firstmove = False
+        self.potentialenPassant = False
         
-    def canMove(self,board,sx,sy, ex,ey):
-        if (sy == ey) and ey == (sy +1) or ey == (sy+2):
-            return True
+        
+    def __repr__(self):
+        return self.symbol.upper() if self.colour == 1 else self.symbol.lower()
+        
+    def safeAccess(self, board, row, col):
+        try:
+            return board[row][col]
+        except IndexError:
+            return None
+        
+    #determine what square the pawn can move too and return as a list
+    def moves(self, board):
+        moves = []
+        
+        #All moves no filter
+        if self.firstmove:
+            forwardMove = [
+                [self.row + (1 * self.colour), self.col],
+                [self.row + 2 * self.colour ,self.col]
+            ]
         else:
-            return False
+            forwardMove = [[self.row + (1 * self.colour), self.col]]
             
+        captureMove = [
+            [self.row + self.colour, self.col + 1],
+            [self.row + self.colour, self.col - 1]
+        ]
+        
+        enPassant = [
+            [self.row,self.col + 1],
+            [self.row,self.col - 1]
+        ]
+        
+        
+        
+        
+        #VALIDATE MOVES 
+        validForward = []
+        
+        for move in forwardMove:
+            row,col = move
+            if row == 8 or row == -1:
+                break;
+            if board[row][col] != None:
+                break;
+            else:
+                validForward.append(move)
+        
+        
+        
+        validCaptures = []
+        for capture in captureMove:
+            row,col = capture
+            piece = self.safeAccess(board, row, col)
+            if piece is None:
+                continue
+            if piece.colour != self.colour:
+                validCaptures.append(capture)
+        
+        
+        validEnpassant=[]
+        
+        for capture in enPassant:
+            row,col = capture
+            piece = self.safeAccess(board, row, col)
+            if piece is None:
+                continue
+            if piece.colour != self.colour and piece.potentialenPassant == True:
+                validEnpassant.append(capture)
+        
+        moves.extend(validForward)
+        moves.extend(validCaptures)
+        moves.extend(validEnpassant)
+        #can this so the for loops append directly to moves but fine for now
+        print(moves)
+        
+        
     
